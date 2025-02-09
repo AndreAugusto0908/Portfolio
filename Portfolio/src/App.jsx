00471613283b8cc17886c255d2697085f9e69fac
Andre <andreaugustosilvacarvalho@gmail.com>
2025-02-09 01:17:12 -0300

    feat/Tecnologias Adicionadas

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,21 +4,22 @@ import Hero from "./components/Hero"
 import Contact from "./components/Contact"
 import Tech from "./components/Tech"
 
+
 function App() {
   return (
     <>
-      <div class="fixed -z-10 min-h-screen w-full
-          [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <div className="fixed inset-0 -z-10 h-screen w-screen bg-[radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="relative min-h-screen w-full overflow-auto">
         <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
-          <Navbar></Navbar>
-          <Hero></Hero>
-          <Tech></Tech>
-          <Projects></Projects>
-          <Contact></Contact>
+          <Navbar />
+          <Hero />
+          <Tech />
+          <Projects />
+          <Contact />
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Portfolio/src/components/Tech.jsx
+++ Portfolio/src/components/Tech.jsx
@@ -1,7 +1,34 @@
+import { BiLogoJava, BiLogoJavascript, BiLogoReact, BiLogoTailwindCss } from "react-icons/bi";
+import { SiDotnet, SiMysql, SiSpring } from "react-icons/si";
+
+const techs = [
+  { icon: BiLogoJava, color: "text-orange-500" },
+  { icon: BiLogoJavascript, color: "text-yellow-400" },
+  { icon: BiLogoTailwindCss, color: "text-sky-400" },
+  { icon: BiLogoReact, color: "text-blue-500" },
+  { icon: SiMysql, color: "text-blue-500" },
+  { icon: SiSpring, color: "text-green-500" },
+  { icon: SiDotnet, color: "text-blue-700" }
+];
+
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div id="tech" className="flex min-h-screen w-full flex-col items-center justify-center gap-16 md:gap-32">
 
-export default Tech
+      <h1 className="text-4xl font-light text-white md:text-6xl">
+        Tecnologias
+      </h1>
+
+      <div className="flex flex-wrap items-center justify-center gap-10 p-5">
+        {techs.map(({ icon: Icon, color }, index) => (
+          <div key={index}>
+            <Icon className={`cursor-pointer text-[80px] ${color} transition-all duration-300
+            hover:-translate-y-5 sm:text-[100px] md:text-[120px]`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tech;
